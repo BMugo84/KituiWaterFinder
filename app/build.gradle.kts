fb085390d0a2fac13972afd18c78ed8aa9a3a3ec
BMugo84 <mugo.benson.kbm@gmail.com>
2026-02-10 11:56:47 -0600

    D. Add Firebase Plugin Open your project-level build.gradle.kts (or build.gradle) file and add the Google services plugin:

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services") // We'll add this after Firebase setup
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false // Add this line
 }
 
 android {
