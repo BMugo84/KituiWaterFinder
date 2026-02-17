786de5f353cd2b0f57ac87071d3f2b054c397fcb
BMugo84 <mugo.benson.kbm@gmail.com>
2026-02-17 04:56:51 -0600

    PHASE 6: CLEANUP AND POLISH We'll improve:
    App Theme - Better colors and styling
    App Icon Name - Shows correct name on phone
    Remove Test Code - Clean up anything we don't need
    Final Code Review - Make sure everything is tidy
    
    Step 6.1: Create a Proper Theme
    
    Make sure you have a ui/theme package
    Create Color.kt in that package:
    Create Theme.kt in the same package:

--- app/src/main/java/com/example/kituiwaterfinder/ui/theme/Color.kt
+++ app/src/main/java/com/example/kituiwaterfinder/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.example.kituiwaterfinder.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary colors - Blue (representing water)
+val Blue80 = Color(0xFF1565C0)
+val Blue60 = Color(0xFF1976D2)
+val Blue40 = Color(0xFF42A5F5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary colors
+val Teal80 = Color(0xFF00695C)
+val Teal60 = Color(0xFF00897B)
+val Teal40 = Color(0xFF4DB6AC)
+
+// Status colors
+val StatusGreen = Color(0xFF4CAF50)
+val StatusRed = Color(0xFFF44336)
+
+// Neutral colors
+val BackgroundLight = Color(0xFFF5F5F5)
+val SurfaceLight = Color(0xFFFFFFFF)
